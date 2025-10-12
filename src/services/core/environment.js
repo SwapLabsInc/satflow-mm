6ca299b473691bdf99474538cab8e452ccc94ba6
RobertClarke <robert@rjfc.net>
2025-10-12 14:59:43 -0400

    Revert staging back to main

--- src/services/core/environment.js
+++ src/services/core/environment.js
@@ -2,7 +2,7 @@ const { decrypt } = require('./encryption');
 const { logError } = require('../../utils/logger');
 
 // API Base URL - change this single line to test against different endpoints
-const SATFLOW_API_BASE_URL = 'https://api-staging.satflow.com/v1';
+const SATFLOW_API_BASE_URL = 'https://api.satflow.com/v1';
 
 // Global flag to track if below-floor confirmation has been given
 let belowFloorConfirmationGiven = false;
